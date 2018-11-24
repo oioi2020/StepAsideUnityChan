caf0641ba8cf229d85c354a71feb454052fb8722
oike.takeshi <o2gpsh@gmail.com>
2018-11-24 22:40:38 +0900

    Lesson6  課題　20181124（初回提出）
    『課題』『発展課題』共に進めています。
    よろしくお願いします。

---
+++ Assets/DestroyController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyController : MonoBehaviour {
+
+	//Unityちゃんのオブジェクト
+	private GameObject unitychan;
+
+	// Use this for initialization
+	void Start () {
+
+		//Unityちゃんのオブジェクトを取得
+		unitychan = GameObject.Find ("unitychan");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//unityちゃんの後ろに移動したら、消失する
+		if (unitychan.transform.position.z > this.transform.position.z + 5){
+
+			//ゲームオブジェクト（自らの消去）
+			Destroy (gameObject);
+
+		}
+		
+	}
+}
